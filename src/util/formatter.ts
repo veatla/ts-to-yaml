14b243c7d6be8acd76cdc6f9c576ff41e932e422
altaev <altaev@clout.one>
2023-07-07 13:54:26 +0300

    switching to TypeScript parser

--- src/util/formatter.ts
+++ src/util/formatter.ts
@@ -1,121 +1,207 @@
-const tab = "  ";
-const regexJsDocComments = /\/\*\*[^*/]*\*\//gm;
-const regexDefaultComments = /\/\/[^\n]*/gm;
-const regexMultiLineComments = /\/\*[^*/]*\*\//;
-const regexTypescriptItem = /(type|enum|interface)\s(\w+)\s(=\s)?\{[^}]*}/gm;
-const regexTypescriptTitle = /(type|interface|enum)\s(\w+)/;
-const regexKeyAndDefinition =
-      /\s*\w+(\??:|[\s=]+)(\s?\w+(\[\])?)+(\s?\|\s?\w+(\[\])?)*/gm;
+import {
+      createSourceFile,
+      ScriptTarget,
+      ScriptKind,
+      isEnumDeclaration,
+      isInterfaceDeclaration,
+      isTypeAliasDeclaration,
+      EnumDeclaration,
+      InterfaceDeclaration,
+      TypeAliasDeclaration,
+      MappedTypeNode,
+      TypeElement,
+      TypeNode,
+      SourceFile,
+} from "typescript";
+const tab = (count = 1) => "  ".repeat(count);
+// const regexJsDocComments = new RegExp(/\/\*\*[^*/]*\*\//gm);
+// const regexDefaultComments = new RegExp(/\/\/[^\n]*/gm);
+// const regexMultiLineComments = new RegExp(/\/\*[^*/]*\*\//);
+// const regexTypescriptItem = new RegExp(
+//       /(type|enum|interface)\s+(\w+)\s+(=\s+)?\{[^}]*}/gm
+// );
+// const regexTypescriptTitle = new RegExp(/(type|interface|enum)\s+(\w+)/);
+// const regexKeyAndDefinition = new RegExp(
+//       /([^?\n:=]+)\s*(\??:|=){1}\s*(["'`\w*\s]{2,}(\s*\|\s*[\w\s"'`]+)*)/g
+// );
+type PropertyItemType = {
+      keyName: string;
+      definitions: string[];
+      types: string[];
+      items: string;
+      isRequired: boolean;
+      isLiteral: boolean;
+};
+type InterfaceItem = {
+      title: string;
+      type: "enum" | "type" | "interface";
+      properties: PropertyItemType[];
+      requiredList: string[];
+};
 
-function clearComments(value: string) {
-      return value
-            .replace(regexMultiLineComments, "")
-            .replace(regexJsDocComments, "")
-            .replace(regexDefaultComments, "");
-}
+const typesList = new Map<string, InterfaceItem>();
+const TypescriptItems = new Map<string, InterfaceItem>();
+
 export function typeFormatter(tsValue: string) {
-      const cleared = clearComments(tsValue);
+      typesList.clear();
       const val = {
             value: "",
       };
-      const matched = cleared.match(regexTypescriptItem);
-      if (matched) {
-            for (const typeString of matched) {
-                  const typeTitle = regexTypescriptTitle.exec(typeString);
-                  const ts = {
-                        title: "",
-                        type: "",
-                        properties: "",
-                        required: "",
-                  };
-                  if (typeTitle && typeTitle[2]) {
-                        ts.title = typeTitle[2];
+      const sourceFile = createSourceFile(
+            "dummy.ts",
+            tsValue,
+            ScriptTarget.Latest,
+            false,
+            ScriptKind.TS
+      );
+      for (const node of sourceFile.statements) {
+            const isValid =
+                  isEnumDeclaration(node) ||
+                  isInterfaceDeclaration(node) ||
+                  isTypeAliasDeclaration(node);
+            if (isValid) {
+                  if ("type" in node && "members" in node.type) {
+                        typeMapper(node, sourceFile);
                   }
-                  const matchedKeysList = typeString
-                        .replace(regexTypescriptTitle, "")
-                        .match(regexKeyAndDefinition);
-                  if (matchedKeysList) {
-                        for (const matchedKey of matchedKeysList) {
-                              const val = getKeyAndValue(
-                                    matchedKey,
-                                    (typeTitle && typeTitle[1]) === "enum"
-                              );
-                              if (val) {
-                                    if (val.required)
-                                          ts.required += `\n${tab}${tab}- ${val.required}`;
+            }
+      }
+      for (const [key, value] of typesList) {
+            val.value += `${key}:\n`;
+            val.value += `${tab()}properties:\n`;
+            const isEnum = value.type === "enum";
+            if (isEnum) {
+                  val.value += enumToString(value.properties);
+            } else {
+                  for (const type of value.properties) {
+                        val.value += propertyObjectToString(type);
+                  }
+                  if (value.requiredList.length) {
+                        val.value += `${tab(1)}required:\n`;
+                        for (const type of value.requiredList) {
+                              val.value += `${tab(2)}- ${type}\n`;
+                        }
+                  }
+            }
+            val.value += `\n`;
+      }
+      return val.value;
+}
 
-                                    if (ts.properties.length)
-                                          ts.properties += `${tab}`;
-                                    ts.properties += `${val.value}`;
+function typeMapper(node: TypeAliasDeclaration, sourceFile: SourceFile) {
+      const mappedTypeNode = node.type as MappedTypeNode;
+      const name = node.name.text;
+      const item: InterfaceItem = {
+            properties: [],
+            requiredList: [],
+            title: name,
+            type: "type",
+      };
+      for (const member of mappedTypeNode.members ?? []) {
+            const memberName = member?.name?.getText(sourceFile);
+            const property: PropertyItemType = {
+                  definitions: [],
+                  isLiteral: false,
+                  isRequired: false,
+                  items: "",
+                  keyName: `${memberName}`,
+                  types: [],
+            };
+            const type = "type" in member ? (member.type as TypeNode) : null;
+            if (memberName) {
+                  const definitions = type
+                        ?.getText(sourceFile)
+                        .split(/\s*\|\s*/gm);
+                  if (!member?.questionToken) {
+                        item.requiredList.push(memberName);
+                  }
+                  if (definitions?.length) {
+                        for (const definition of definitions) {
+                              const check = checkType(definition);
+                              property.types.push(check.type);
+                              if (check.isLiteral) {
+                                    property.isLiteral = check.isLiteral;
                               }
                         }
                   }
-                  if (val.value.length) val.value += `\n\n`;
-                  val.value += generateNewObject(ts);
+            }
+            item.properties.push(property)
+      }
+      typesList.set(name, item);
+}
+function enumToString(properties: PropertyItemType[]) {
+      const type = [...new Set(properties.flatMap((v) => v.types))];
+      const definitions = [
+            ...new Set(properties.flatMap((v) => v.definitions)),
+      ];
+      const te = {
+            xt: "",
+      };
+
+      if (type.length > 1) {
+            te.xt += `${tab(3)}oneOf:\n`;
+            for (const t of type) {
+                  te.xt += `${tab(4)}- type: ${t}\n`;
+            }
+      } else {
+            te.xt += `${tab(3)}type: ${type[0]}\n`;
+      }
+      te.xt += `${tab(3)}enum:\n`;
+      for (const definition of definitions) {
+            te.xt += `${tab(4)}- ${definition.trim()}\n`;
+      }
+      return te.xt;
+}
+function propertyObjectToString(properties: PropertyItemType) {
+      const { isLiteral, items, keyName, definitions, types } = properties;
+      const te = {
+            xt: "",
+      };
+
+      const type = [...new Set(types)];
+      te.xt += `${tab(2)}${keyName}:\n`;
+
+      if (type.length > 1) {
+            te.xt += `${tab(3)}oneOf:\n`;
+            for (const t of type) {
+                  te.xt += `${tab(4)}- type: ${t}\n`;
+            }
+      } else {
+            te.xt += `${tab(3)}type: ${type[0]}\n`;
+      }
+
+      if (isLiteral) {
+            te.xt += `${tab(3)}enum:\n`;
+            for (const definition of definitions) {
+                  te.xt += `${tab(4)}- ${definition}\n`;
             }
       }
-
-      return val.value;
-}
-function generateNewObject({
-      properties,
-      required,
-      title,
-}: {
-      title: string;
-      properties: string;
-      required: string;
-}) {
-      const val = {
-            value: "",
-      };
-
-      // Adding Title
-      val.value += `${title}: \n`;
-      // Adding Type
-
-      val.value += `${tab}type: object\n`;
-      if (properties.length) {
-            val.value += `${tab}properties: \n${tab}${properties}\n`;
+      if (items.length) {
+            te.xt += `${tab(2)}items:\n`;
+            te.xt += `${tab(3)}${items}\n`;
       }
 
-      // Adding required list If is exists
-      if (required.length) val.value += `${tab}required: ${required}`;
-      return val.value;
-}
-
-function getKeyAndValue(matchedType: string, isEnum?: boolean) {
-      const val = {
-            value: "",
-            required: null as string | null,
-            enumType: undefined as string | undefined,
-      };
-      const [key, value, enumValue] = matchedType.trim().split(/(\??:|[\s=]+)/gm);
-      if (isEnum) console.log(key, enumValue);
-      const isOptional = matchedType.search(/\?/gm);
-      if (!key || !value) return null;
-      if (isOptional > -1) val.required = key.trim();
-      const type = checkType(value.trim());
-      const items =
-            type === "array"
-                  ? `\n${tab}${tab}${tab}items:\n${tab}${tab}${tab}${tab}$ref: '#/components/schemas/${value
-                          .replace("[]", "")
-                          .trim()}'`
-                  : "";
-      val.value = `${tab}${key.trim()}: \n${tab}${tab}${tab}type: ${type}${items}\n`;
-      return val;
+      return te.xt;
 }
 
 function checkType(value: string) {
-      if (value.includes("string")) return "string";
-      if (value.includes("object")) return "object";
-      else if (value.includes("number")) return "number";
-      else if (value.includes("integer")) return "integer";
-      else if (value.includes("boolean")) return "boolean";
-      else if (value.includes("[]")) return "array";
-      else {
-            return "object";
-      }
+      const item = {
+            type: "undefined",
+            isLiteral: false,
+      };
+      const isLiteralString = /"(?:[^"\\]|\\.)*"/gim.test(value);
+      if (isLiteralString) {
+            item.isLiteral = true;
+            item.type = "string";
+      } else if (!Number.isNaN(Number(value))) {
+            item.isLiteral = true;
+            item.type = "number";
+      } else if (value.includes("string")) item.type = "string";
+      else if (value.includes("object")) item.type = "object";
+      else if (value.includes("number")) item.type = "number";
+      else if (value.includes("integer")) item.type = "integer";
+      else if (value.includes("boolean")) item.type = "boolean";
+      else if (value.includes("[]")) item.type = "array";
+      return item;
       //     throw new Error(
       //       `${value} should be equal to one of the allowed values.`
       //       + `\nAllowed Values: array, boolean, integer, number, object, string`
